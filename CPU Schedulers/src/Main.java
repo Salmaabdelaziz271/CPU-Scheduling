4a82bd8554ed71864ccb8b373a0560e310b31807
Salmaabdelaziz271 <salmayaraaa@gmail.com>
2023-12-07 20:30:59 +0200

    Build CPU Scheduling Classes

--- CPU Schedulers/src/Main.java
+++ CPU Schedulers/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        CPUScheduling s = new SRTFScheduling();
+        List<Process>processes=new ArrayList<>();
+        Process p1 = new Process("P1", "Red", 0, 7,2);
+        Process p2 = new Process("P2", "Red", 2, 4,2);
+        Process p3 = new Process("P3", "Red", 4, 1,2);
+        Process p4 = new Process("P4", "Red", 5, 4,2);
+        processes.add(p1);
+        processes.add(p2);
+        processes.add(p3);
+        processes.add(p4);
+        s.printExecutionOrder(processes);
 
-        System.out.println("Hello world!");
     }
 }
--- CPU Schedulers/src/SRTFScheduling.java
+++ CPU Schedulers/src/SRTFScheduling.java
@@ -1,10 +1,40 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class SRTFScheduling implements CPUScheduling{
     @Override
     public void printExecutionOrder(List<Process> processes) {
+        PriorityQueue<Process> readyQueue = new PriorityQueue<>(new Comparator<Process>() {
+            @Override
+            public int compare(Process o1, Process o2) {
+                if (o1.burstTime < o2.burstTime) {
+                    return -1;
+                } else if (o1.burstTime > o2.burstTime) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        List<Process> finishedProcesses = new ArrayList<>();
+        double currentTime = 0;
+        while (finishedProcesses.size() < processes.size()) {
+            for (Process p : processes) {
+                if (p.arrivalTime <= currentTime && !finishedProcesses.contains(p)) {
+                    readyQueue.add(p);
+                }
+            }
+            Process currentProcess = readyQueue.poll();
+            currentProcess.finishTime = currentTime + currentProcess.burstTime;
+            currentTime = currentProcess.finishTime;
+            finishedProcesses.add(currentProcess);
+        }
+
 
     }
+
     @Override
     public double calculateTurnaroundTime(Process process) {
         double turnaroundTime = process.finishTime - process.arrivalTime;
